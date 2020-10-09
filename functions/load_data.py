ebaaed2dfb49fee372c9abb22c31584dee5c523a
Anna Youngyeun <anna.youngyeun@gmail.com>
2020-10-09 18:21:24 -0500

    Add new movies

--- functions/load_data.py
+++ functions/load_data.py
@@ -3,17 +3,19 @@ import pandas as pd
 import sqlite3
 from sqlalchemy import create_engine
 
-data_frame = pd.read_csv('./data/movie_plots.csv')
-updated_df = data_frame.rename(columns={"Release Year": "ReleaseYear",
-                                        "Origin/Ethnicity": "OriginEthnicity", "Wiki Page": "WikiPage"})
 
-# Clear db if exists so data is fresh
-if os.path.exists('database.db'):
-    os.remove('database.db')
+def load_data():
+    data_frame = pd.read_csv('./data/movie_plots.csv')
+    updated_df = data_frame.rename(columns={"Release Year": "ReleaseYear",
+                                            "Origin/Ethnicity": "OriginEthnicity", "Wiki Page": "WikiPage"})
 
-engine = create_engine('sqlite:///database.db', echo=True)
-sql_connection = engine.connect()
-updated_df.to_sql('Movies', sql_connection)
+    # Clear db if exists so data is fresh
+    if os.path.exists('database.db'):
+        os.remove('database.db')
 
-sql_connection.row_factory = sqlite3.Row
-sql_connection.close()
+    engine = create_engine('sqlite:///database.db', echo=True)
+    sql_connection = engine.connect()
+    updated_df.to_sql('Movies', sql_connection)
+
+    sql_connection.row_factory = sqlite3.Row
+    sql_connection.close()
